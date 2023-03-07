3c17a060df7b6aed0331d5c08f6e0afcdb3de156
soonable@naver.com <soonable@naver.com>
2023-03-07 10:45:10 +0900

    feat : 리팩토링 전 야구게임 proto version

--- src/BaseballGameApp.java
+++ src/BaseballGameApp.java
@@ -1,7 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class BaseballGameApp {
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame();
-        baseballGame.start();
-        baseballGame.end();
+        String input = "";
+        int min = 1;
+        int max = 9;
+        String number1 = String.valueOf((int) (Math.random() * (max - min + 1)) + min);
+        String number2 = String.valueOf((int) (Math.random() * (max - min + 1)) + min);
+        String number3 = String.valueOf((int) (Math.random() * (max - min + 1)) + min);
+
+        String randomNumber = number1 + number2 + number3;
+
+        while (!input.equals(randomNumber)) {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            System.out.println("숫자를 입력해 주세요 : " + input);
+
+            validateInput(input, randomNumber);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        Scanner scanner = new Scanner(System.in);
+        String startOrEnd = scanner.nextLine();
+        if(startOrEnd.equals("1")) {
+            main(args);
+        } else {
+            scanner.close();
+        }
+    }
+
+    private static void validateInput(String input, String number) {
+        ArrayList<Character> arr = new ArrayList<>();
+        for(int i = 0; i < number.length(); i++) {
+            arr.add(number.charAt(i));
+        }
+
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (input.charAt(i) == number.charAt(i)) {
+                strike++;
+                continue;
+            }
+            if(arr.contains(input.charAt(i))) {
+                ball++;
+            }
+        }
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball > 0 && strike > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(strike + "스트라이크");
+        }
     }
 }
