d1a149443c9c6dfff2bd1d31a2791db9e9b72e85
soonable@naver.com <soonable@naver.com>
2023-03-07 14:47:06 +0900

    refactor : play 로직 BaseballGame 클래스에서 구현

--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -4,24 +4,15 @@ public class BaseballGame {
     String computerNumber;
     String userNumber;
 
-    void createComputerNumber() {
-        int min = 1;
-        int max = 9;
-        String number1 = String.valueOf((int)(Math.random() * (max - min +1)) + min);
-        String number2 = String.valueOf((int)(Math.random() * (max - min +1)) + min);
-        String number3 = String.valueOf((int)(Math.random() * (max - min +1)) + min);
-        setComputerNumber(number1+number2+number3);
-    }
-
-    void play() {
-        createComputerNumber();
-        String numbers = input();
+    void play(String input, String randomNumber) {
         BaseballValidator baseballValidator = new BaseballValidator();
-    }
-
-    String input() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        while (!input.equals(randomNumber)) {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            baseballValidator.validateInput(input);
+            System.out.println("숫자를 입력해 주세요 : " + input);
+            baseballValidator.decideBallAndStrike(input, randomNumber);
+        }
     }
 
     void end() {
--- src/BaseballGameApp.java
+++ src/BaseballGameApp.java
@@ -1,19 +1,14 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BaseballGameApp {
     public static void main(String[] args) {
         String input = "";
         RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
         String randomNumber = randomNumberGenerator.createDistinctRandomNumber();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.play(input, randomNumber);
 
-        while (!input.equals(randomNumber)) {
-            Scanner scanner = new Scanner(System.in);
-            input = scanner.nextLine();
-            System.out.println("숫자를 입력해 주세요 : " + input);
-
-            validateInput(input, randomNumber);
-        }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
@@ -23,36 +18,6 @@ public class BaseballGameApp {
             main(args);
         } else {
             scanner.close();
-        }
-    }
-
-    private static void validateInput(String input, String number) {
-        ArrayList<Character> arr = new ArrayList<>();
-        for(int i = 0; i < number.length(); i++) {
-            arr.add(number.charAt(i));
-        }
-
-        int ball = 0;
-        int strike = 0;
-
-        for (int i = 0; i < 3; i++) {
-            if (input.charAt(i) == number.charAt(i)) {
-                strike++;
-                continue;
-            }
-            if(arr.contains(input.charAt(i))) {
-                ball++;
-            }
-        }
-
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
-        } else if (ball > 0 && strike > 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        } else if (strike == 0) {
-            System.out.println(ball + "볼");
-        } else {
-            System.out.println(strike + "스트라이크");
         }
     }
 }
