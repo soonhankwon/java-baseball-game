25a792b04d165e377c1e4e8cfa6765b88c3f1ed6
soonable@naver.com <soonable@naver.com>
2023-03-09 18:26:42 +0900

    refactor : 입력값의 유효성 검사를 Ball 클래스의 내부에서 하도록 변경

--- src/baseball/Ball.java
+++ src/baseball/Ball.java
@@ -1,12 +1,21 @@
 package baseball;
 
 public class Ball {
+    public static final int MIN_NO = 1;
+    public static final int MAX_NO = 9;
     private final int position;
     private final int ballNo;
 
     public Ball(int position, int ballNo) {
+        if(!isBallValid(ballNo)) {
+            throw new IllegalArgumentException("입력값은 1~9까지의 숫자입니다.");
+        }
         this.position = position;
         this.ballNo = ballNo;
+    }
+
+    private boolean isBallValid(int ballNo) {
+        return ballNo >= MIN_NO && ballNo <= MAX_NO;
     }
 
     public BallStatus play(Ball ball) {
@@ -18,7 +27,6 @@ public class Ball {
         }
         return BallStatus.NOTHING;
     }
-
     private boolean matchBallNo(int ballNo) {
         return this.ballNo == ballNo;
     }
